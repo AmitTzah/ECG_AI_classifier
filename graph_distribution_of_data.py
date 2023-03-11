84733c2fac7e0a923fcf6d2796d3d859961942ac
amittzah <amitq1324@gmail.com>
2023-03-11 20:12:36 +0200

    Seperate into three figures

--- graph_distribution_of_data.py
+++ graph_distribution_of_data.py
@@ -18,9 +18,9 @@ def get_diagnoses(folder_path):
     return diagnoses
 
 
-folder1 = "./test_code/data"
-folder2 = "./test_code/training_folder"
-folder3 = "./test_code/validation_folder"
+folder1 = "data"
+folder2 = "training_folder"
+folder3 = "validation_folder"
 
 diagnoses1 = get_diagnoses(folder1)
 diagnoses2 = get_diagnoses(folder2)
@@ -56,27 +56,35 @@ for dx in dx_codes2 + dx_codes3:
 dx_order.sort()
 
 # Create a bar chart for each folder
-fig, axs = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
-axs[0].bar(dx_order, [diagnoses1.get(dx, 0) for dx in dx_order])
-axs[1].bar(dx_order, [diagnoses2.get(dx, 0) for dx in dx_order])
-axs[2].bar(dx_order, [diagnoses3.get(dx, 0) for dx in dx_order])
+fig1, ax1 = plt.subplots(figsize=(10, 5))
+fig2, ax2 = plt.subplots(figsize=(10, 5))
+fig3, ax3 = plt.subplots(figsize=(10, 5))
+
+ax1.bar(dx_order, [diagnoses1.get(dx, 0) for dx in dx_order])
+ax2.bar(dx_order, [diagnoses2.get(dx, 0) for dx in dx_order])
+ax3.bar(dx_order, [diagnoses3.get(dx, 0) for dx in dx_order])
 
 # Set the titles and axis labels
-axs[0].set_title(f"{folder1}")
-axs[1].set_title(f"{folder2}")
-axs[2].set_title(f"{folder3}")
+ax1.set_title(f"{folder1}")
+ax2.set_title(f"{folder2}")
+ax3.set_title(f"{folder3}")
 
-fig.suptitle("Diagnostic Code Histograms")
-plt.xlabel("Diagnostic Code")
-plt.ylabel("Number of Patients")
+fig1.suptitle("Diagnostic Code Histograms")
+fig2.suptitle("Diagnostic Code Histograms")
+fig3.suptitle("Diagnostic Code Histograms")
 
+ax1.set_xlabel("Diagnostic Code")
+ax1.set_ylabel("Number of Patients")
+ax2.set_xlabel("Diagnostic Code")
+ax2.set_ylabel("Number of Patients")
+ax3.set_xlabel("Diagnostic Code")
+ax3.set_ylabel("Number of Patients")
 
 # Add the top 7 diagnoses as text annotations to each chart
-for i, ax in enumerate(axs):
+for i, ax in enumerate([ax1, ax2, ax3]):
     text = "\n".join([f"{dx}: {count}" for dx, count in [
                      top_diagnoses1, top_diagnoses2, top_diagnoses3][i]])
     ax.text(0.5, 0.95, text, transform=ax.transAxes, va='top', ha='center')
-
 
 # use the Dx_map.csv file to get the full name of the diagnosis
 # and add it to the chart
@@ -85,9 +93,8 @@ with open("Dx_map.csv", 'r') as f:
               for line in f.readlines()}
 
 
-for i, ax in enumerate(axs):
+for i, ax in enumerate([ax1, ax2, ax3]):
     for dx, count in [top_diagnoses1, top_diagnoses2, top_diagnoses3][i]:
         ax.text(dx, count, dx_map[dx], ha='center', va='bottom')
-
 
 plt.show()
